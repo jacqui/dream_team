31a84fe0ecd0430063402414e22dcb06d71bca4c
Jacqui Maher <jacqui.maher@nytimes.com>
2012-11-29 11:34:24 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DreamTeam::Application.config.secret_token = '5fda3c89fcc005e9c6247cc94415426e625ff9bb0b5a4858e37b6ad8f69a22ff5d5f2ff8ce4edaf8d3b9f39b40cddff0fea0d539ff6994726a6ad97647a55be7'
