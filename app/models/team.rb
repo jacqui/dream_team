b41a02a339c0b3147ad7b571aee213a5ec34af62
Jacqui Maher <jacqui.maher@nytimes.com>
2012-11-29 15:38:31 -0500

    Can't mass-assign my...

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
 class Team < ActiveRecord::Base
-  attr_accessible :name, :color, :web_link, :api_link, :news_api_link, :notes_api_link, :mobile_link
+  attr_accessible :name, :color, :web_link, :api_link, :news_api_link, :notes_api_link, :mobile_link, :espn_id
   has_many :players
 end
