29c6022274d9397923155463151370da8b8e8546
Jacqui Maher <jacqui.maher@nytimes.com>
2012-12-02 19:43:50 -0500

    Updated db seeds to pull in player data from football nerd's api

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 require 'espn_api'
+require 'football_nerd'
 
+Team.destroy_all
+Player.destroy_all
+
+puts "Loading teams from the ESPN API..."
 espn = EspnApi.new('football')
 espn.load_teams
-espn.load_players
+puts "Done loading teams from the ESPN API. We now have #{Team.count} teams stored!"
+
+puts "Loading players from the Football Nerd API..."
+nerd = FootballNerd.new
+nerd.load_players
+puts "Done loading players from the Football Nerd API. We now have #{Player.count} players stored!"
