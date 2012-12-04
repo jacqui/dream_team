63948f63e6880c0a48b74644614914555266a51e
Jacqui Maher <jacqui.maher@nytimes.com>
2012-12-04 09:12:21 -0500

    Moved duplicated code to module for reader auth and project setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  include AuthFilters
   before_filter :set_project, :set_reader
-
-  def set_project
-    @project = Project.find_by_slug(params[:project])
-    if @project.blank?
-      @project = Project.first || Project.create!(
-        :name => "Super Bowl 2013",
-        :slug => "super-bowl-2013"
-      )
-    end
-    self.default_url_options[:project] = @project
-  end
-
-  # Hard code for now.
-  # TODO: Make this transparent, possibly use code from chad / extract into
-  # gem.
-  def set_reader
-    @reader = Reader.first || Reader.create!(
-      :first_name => "Michael",
-      :last_name => "Strickland"
-    )
-  end
 
 end
