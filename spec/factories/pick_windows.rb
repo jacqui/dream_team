340b1d81d83f461276c72f5f3e76b35049cd13f6
Jacqui Maher <jacqui.maher@nytimes.com>
2012-12-23 17:26:45 -0500

    Removed unused specs and factories, this is a prototype

--- spec/factories/pick_windows.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :pick_window do
-    window_start "2012-11-29 14:28:18"
-    window_end "2012-11-29 14:28:18"
-    project_id 1
-  end
-end
